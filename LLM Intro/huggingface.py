21b009e242da1397bb892b9ca840e8a3e0941eb3
Sujal Anand <suanan@akamai.com>
2024-06-30 16:45:23 +0530

    push changes

--- LLM Intro/huggingface.py
+++ LLM Intro/huggingface.py
@@ -8,9 +8,9 @@
 #from langchain-huggingface.llms import HuggingFaceHub
 from langchain.llms import HuggingFaceHub
 import os
-#please add API key code below for running the code, as the code scans are giving issue
-llm = HuggingFaceHub(repo_id="google/flan-t5-large")
 
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
+llm = HuggingFaceHub(repo_id="google/flan-t5-large")
 our_query = "what is the currency of India"
 llm = llm(our_query)
 print("Currency is " + llm)
--- LLM Intro/openai.py
+++ LLM Intro/openai.py
@@ -2,6 +2,8 @@
 from langchain.llms import openAI
 import os
 #please add API key code below for running the code, as the code scans are giving issue
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = ""
+
 llm = openAI(model_name = "text-davinci-003")
 our_query = "What is the currency of India?"
 response = llm(our_query)
